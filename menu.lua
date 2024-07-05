90a72e0f834f493b6663b08fcf8898cc2de2c1ac
THALESDEV2 <143728518+THALESDEV2@users.noreply.github.com>
2024-07-05 18:19:02 -0300

    Update menu.lua

--- menu.lua
+++ menu.lua
@@ -47,4 +47,18 @@ inicio:AddButton({
                 v.Parent = game:GetService('Players').LocalPlayer.Backpack
             end
         end
+        
+        game:GetService('Players').LocalPlayer.Character.Humanoid.Died:Connect(function()
+            for i, v in pairs(game:GetService('Players').LocalPlayer.Backpack:GetDescendants()) do
+                if v:IsA('Tool') then
+                    v.Parent = game:GetService('Teams')
+                end
+            end
+            for i, v in pairs(game:GetService('Players').LocalPlayer.Character:GetDescendants()) do
+                if v:IsA('Tool') then
+                    v.Parent = game:GetService('Teams')
+                end
+            end
+        end)
+      end    
 })
